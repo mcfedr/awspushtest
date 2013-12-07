807bc3cb4ea7896314f10c47af79b660a2b3bac9
Fred Cox <mcfedr@gmail.com>
2013-12-08 01:06:47 +0200

    creating a bundle to use aws for mobile push

---
+++ src/ekreative/PushBundle/Exception/PlatformNotConfiguredException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mcfedr
+ * Date: 07/12/2013
+ * Time: 14:02
+ */
+
+namespace ekreative\PushBundle\Exception;
+
+
+class PlatformNotConfiguredException extends \Exception {
+
+}
---
+++ src/ekreative/PushBundle/ekreativePushBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ekreative\PushBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ekreativePushBundle extends Bundle
+{
+}
